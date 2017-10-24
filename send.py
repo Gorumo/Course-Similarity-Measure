46959e9c1c797eb23d9282782e677a7b19300f19
Aleksei Romanov <Gloomspb@gmail.com>
2017-10-24 10:00:43 +0300

    json

--- send.py
+++ send.py
@@ -4,8 +4,8 @@ url = 'http://localhost:9000/similarity/gades'
 data = {
 "tasks": 
 [{
-"uri1" : 'http://www.semanticweb.org/EdxOntology/Main#5964fec656c02c4382a94a3d', 
-"uri2" : 'http://www.semanticweb.org/EdxOntology/Main#59651a0f56c02c021ca949f6'
+"uri1" : 'http://www.semanticweb.org/MOOCOntology#CS101_Математика', 
+"uri2" : 'http://www.semanticweb.org/MOOCOntology#CS100500_Продвинутая_математика'
 }]}
 r = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
 print(r.status_code)
